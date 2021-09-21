383e69001a006ca9ce165b82a519ab176d91183a
Tania Pitikova <tetiana.pitikova@gmail.com>
2021-09-21 14:02:32 +0300

    adding js scripts (sliders, up button and site header transition)

---
+++ js/script.js
@@ -0,0 +1,141 @@
+// slider1 start //
+window.addEventListener("load", () => {
+    var carousels = document.querySelectorAll(".carousel-3d");
+    for (var i = 0; i < carousels.length; i++) {
+        carousel(carousels[i]);
+    }
+});
+function carousel(root) {
+    var figure = root.querySelector("figure"),
+    nav = root.querySelector("nav"),
+    images = figure.children,
+    n = images.length,
+    gap = root.dataset.gap || 0,
+    bfc = "bfc" in root.dataset,
+    theta = 2 * Math.PI / n,
+    currImage = 0;
+    setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    window.addEventListener("resize", () => {
+        setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    });
+    setupNavigation();
+    function setupCarousel(n, s) {
+        var apothem = s / (2 * Math.tan(Math.PI / n));
+        figure.style.transformOrigin = `50% 50% ${-apothem}px`;
+        for (var i = 0; i < n; i++) images[i].style.padding = `0 ${gap}px`;
+        for (i = 0; i < n; i++) {
+            images[i].style.transformOrigin = `50% 50% ${-apothem}px`;
+            images[i].style.transform = `rotateY(${i * theta}rad)`;
+        }
+        if (bfc)
+        for (i = 0; i < n; i++) images[i].style.backfaceVisibility = "hidden";
+        rotateCarousel(currImage);
+    }
+    function setupNavigation() {
+        nav.addEventListener("click", onClick, true);
+        function onClick(e) {
+            e.stopPropagation();
+            var t = e.target;
+            if (t.tagName.toUpperCase() != "BUTTON") return;
+            if (t.classList.contains("next")) {
+                currImage++;
+                } else {
+                currImage--;
+            }
+            rotateCarousel(currImage);
+        }
+    }
+    function rotateCarousel(imageIndex) {
+        figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
+    }
+
+    let cur;
+    function showcur() {
+        cur = setInterval(function() {
+            currImage++;
+            rotateCarousel(currImage);
+        }, 2000);
+    }
+    function clearcur() {
+        clearInterval(cur);
+    }
+    root.onmouseover  = function() {
+        clearcur();
+    }  
+    root.onmouseout  = function() {
+        showcur();
+    }     
+    showcur();
+}
+// slider1 end//
+// start slider2 //
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+// end slider2 //
+const nodes = [].slice.call(document.querySelectorAll('li .data'), 0);
+const directions  = { 0: 'top', 1: 'right', 2: 'bottom', 3: 'left' };
+const classNames = ['in', 'out'].map((p) => Object.values(directions).map((d) => `${p}-${d}`)).reduce((a, b) => a.concat(b));
+const getDirectionKey = (ev, node) => {
+    const { width, height, top, left } = node.getBoundingClientRect();
+    const l = ev.pageX - (left + window.pageXOffset);
+    const t = ev.pageY - (top + window.pageYOffset);
+    const x = (l - (width/2));
+    const y = (t - (height/2));
+    return Math.round(Math.atan2(y, x) / 1.57079633 + 5) % 4;
+}
+class Item {
+    constructor(element) {
+        this.element = element;    
+        this.element.addEventListener('mouseover', (ev) => this.update(ev, 'in'));
+        this.element.addEventListener('mouseout', (ev) => this.update(ev, 'out'));
+    }
+    update(ev, prefix) {
+        this.element.classList.remove(...classNames);
+        this.element.classList.add(`${prefix}-${directions[getDirectionKey(ev, this.element)]}`);
+    }
+}
+nodes.forEach(node => new Item(node));
+
+// button up //
+$('body').append('<div class="upbtn"></div>');            
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.upbtn').css({
+            bottom: '15px'
+        });
+        } else {
+        $('.upbtn').css({
+            bottom: '-80px'
+        });
+    }
+});
+$('.upbtn').on('click',function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
+// header //
+$(document).ready(function(){
+	$('#nav-menu').click(function(){
+		$('ul.nav-list').addClass('nav-open').slideToggle('300');
+	});
+});
+
+
+
+
+
